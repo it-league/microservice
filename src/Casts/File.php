b56e854741ef28fb487a3624b96d4ff06b844bb8
Alexander Popov <alexander.popov.gg@gmail.com>
2020-09-01 14:14:24 +0300

    file cast changes

--- src/Casts/File.php
+++ src/Casts/File.php
@@ -6,6 +6,8 @@ namespace ITLeague\Microservice\Casts;
 
 use Exception;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Http\Client\RequestException;
+use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Http;
 
 abstract class File implements CastsAttributes
@@ -24,22 +26,46 @@ abstract class File implements CastsAttributes
         return $value;
     }
 
+    /**
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @param string $key
+     * @param mixed $value
+     * @param array $attributes
+     *
+     * @return array|mixed|string
+     * @throws \Illuminate\Http\Client\RequestException
+     */
     public function set($model, string $key, $value, array $attributes)
     {
-        try {
+        // confirm new file
+        if ($value && $this->force === false) {
+            $response = Http::put(config('microservice.storage_uri') . '/confirm/' . $value, ['permissions' => $this->permissions, 'sizes' => $this->sizes]);
+            static::checkResponse($response, 204);
+        }
 
-            if ($attributes[$key]) {
-                Http::delete(config('microservice.storage_uri') . '/delete/' . $attributes[$key]);
-            }
-
-            if ($this->force === false) {
-                Http::put(config('microservice.storage_uri') . '/confirm/' . $attributes[$key], ['permissions' => $this->permissions, 'sizes' => $this->sizes]);
-            }
-
-        } catch (Exception $e) {
-
+        // delete old file
+        if ($attributes[$key]) {
+            $response = Http::delete(config('microservice.storage_uri') . '/delete/' . $attributes[$key]);
+            static::checkResponse($response, 204);
         }
 
         return $value;
+    }
+
+    /**
+     * @param \Illuminate\Http\Client\Response $response
+     * @param int $success
+     *
+     * @throws \Illuminate\Http\Client\RequestException
+     * @throws \Exception
+     */
+    private static function checkResponse(Response $response, int $success = 200) {
+        if ($response->status() !== $success) {
+            if ($content = json_decode($response->body(), true)) {
+                throw new Exception($content['error']['detail'], $content['error']['code']);
+            } else {
+                throw new RequestException($response);
+            }
+        }
     }
 }
