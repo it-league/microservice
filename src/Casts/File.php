9076f846165af2388c695245ce556666dfcb1077
Alexander Popov <alexander.popov.gg@gmail.com>
2020-09-03 17:13:13 +0300

    file cast changes

--- src/Casts/File.php
+++ src/Casts/File.php
@@ -6,11 +6,11 @@ namespace ITLeague\Microservice\Casts;
 
 use Auth;
 use Exception;
-use GuzzleHttp\Client;
-use GuzzleHttp\Exception\RequestException;
+use Illuminate\Http\Client\RequestException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
 
 abstract class File implements CastsAttributes
@@ -47,31 +47,26 @@ abstract class File implements CastsAttributes
             throw new AuthorizationException('Can`t save file without authorization');
         }
 
-        $http = new Client([
-            'base_uri' => config('microservice.storage_uri'),
-            'headers' => [
-                'x-authenticated-userid' => Auth::id(),
-                'x-authenticated-scopes' => implode(' ', Auth::user()->scope),
-            ]
-        ]);
+        $http = Http::withHeaders([
+            'x-authenticated-userid' => Auth::id(),
+            'x-authenticated-scopes' => implode(' ', (array)Auth::user()->scope),
+        ])->baseUrl(config('microservice.storage_uri'));
 
         try {
 
             // confirm new file
             if ($value && $this->force === false) {
-                $http->put('confirm/' . $value, [
-                    'json' => ['permission' => $this->permission, 'sizes' => $this->sizes]
-                ]);
+                $http->put('confirm/' . $value, ['permission' => $this->permission, 'sizes' => $this->sizes])->throw();
             }
 
             // delete old file
             if (Arr::has($attributes, $key) && Str::length($attributes[$key]) === 36) {
-                $http->delete('delete/' . $attributes[$key]);
+                $http->delete('delete/' . $attributes[$key])->throw();
             }
 
         } catch (RequestException $e) {
 
-            if ($content = json_decode($e->getResponse()->getBody(), true)) {
+            if ($content = json_decode($e->response->body(), true)) {
                 throw new Exception($content['error']['detail'], $content['error']['status']);
             } else {
                 throw new Exception();
