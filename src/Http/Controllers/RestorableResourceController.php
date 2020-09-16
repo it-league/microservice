8b706c99bb261a44f76392d5c7310a17e51c460f
Alexander Popov <alexander.popov.gg@gmail.com>
2020-09-16 16:35:48 +0300

    namespace fix

--- src/Http/Controllers/RestorableResourceController.php
+++ src/Http/Controllers/RestorableResourceController.php
@@ -5,7 +5,7 @@ namespace ITLeague\Microservice\Http\Controllers;
 
 
 use Illuminate\Http\JsonResponse;
-use itleague\microservice\Http\Interfaces\RestorableResourceControllerInterface;
+use ITLeague\Microservice\Http\Interfaces\RestorableResourceControllerInterface;
 
 abstract class RestorableResourceController extends ResourceController implements RestorableResourceControllerInterface
 {
