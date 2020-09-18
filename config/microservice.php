0e765bbad489afdd07fae580168eda4744e402cb
Alexander Popov <alexander.popov.gg@gmail.com>
2020-09-18 14:56:58 +0300

    generate file url

--- config/microservice.php
+++ config/microservice.php
@@ -2,6 +2,8 @@
 
 return [
 
-    'storage_uri' => env('STORAGE_SERVICE_BASE_URI', 'https://storage-nginx/storage'),
+    'storage_uri' => env('STORAGE_SERVICE_BASE_URI', 'https://storage-nginx'),
+    'storage_prefix' => env('STORAGE_SERVICE_PREFIX', 'storage'),
+    'api_uri' => env('API_BASE_URI', 'https://gate.heroleague.ru/'),
     'prefix' => env('SERVICE_PREFIX', 'api')
 ];
