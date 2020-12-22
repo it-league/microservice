d2053baff573b6466a9665eac938155232c27322
Alexander Popov <alexander.popov.gg@gmail.com>
2020-12-22 15:21:32 +0300

    logger trait refactor

--- src/Traits/HasLogger.php
+++ src/Traits/HasLogger.php
@@ -3,54 +3,43 @@
 namespace ITLeague\Microservice\Traits;
 
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Request;
-use Illuminate\Support\Facades\Auth;
 
 /**
  * @mixin \Illuminate\Database\Eloquent\Model
  */
 trait HasLogger
 {
-    protected static function registerLogger()
+    public static function bootHasLogger()
+    {
+        static::registerLogger();
+    }
+
+    private static function info(self $model): array
+    {
+        return [
+            'model' => get_class($model),
+            'id' => (string)$model->getKey(),
+            'user' => Auth::id(),
+            'ip' => Request::ip(),
+            'lang' => App::getLocale(),
+        ];
+    }
+
+    protected static function registerLogger(): void
     {
         static::created(
-            fn($model) => Log::info(
-                'Model created',
-                [
-                    'model' => get_class($model),
-                    'id' => (string) $model->getKey(),
-                    'user' => Auth::id(),
-                    'ip' => Request::ip(),
-                    'lang' => App::getLocale(),
-                ]
-            )
+            fn(self $model) => Log::info('Model created', static::info($model))
         );
 
         static::updated(
-            fn($model) => Log::info(
-                'Model updated',
-                [
-                    'model' => get_class($model),
-                    'id' => (string) $model->getKey(),
-                    'user' => Auth::id(),
-                    'ip' => Request::ip(),
-                    'lang' => App::getLocale(),
-                ]
-            )
+            fn(self $model) => Log::info('Model updated', static::info($model))
         );
 
         static::deleted(
-            fn($model) => Log::info(
-                'Model deleted',
-                [
-                    'model' => get_class($model),
-                    'id' => (string) $model->getKey(),
-                    'user' => Auth::id(),
-                    'ip' => Request::ip(),
-                    'lang' => App::getLocale(),
-                ]
-            )
+            fn(self $model) => Log::info('Model deleted', static::info($model))
         );
     }
 }
